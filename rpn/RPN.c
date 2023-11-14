b893534f1b0ab0ffdccf5bb88ffd49fbab586d09
Love Olsson <lo0016ol-s@student.lu.se>
2023-11-14 20:23:50 +0100

    started rpn

--- rpn/RPN.c
+++ rpn/RPN.c
@@ -6,6 +6,7 @@ int stack[MAX_SIZE];
 int items = -1;
 int error = 0;
 int running = 1;
+int line = 1;
 
 int empty(){
     return items = -1;
@@ -40,13 +41,50 @@ int peek(int* i){
 }
 
 
+
 static int isdigit_etc(int c){
     return isdigit(c) || c == '-' || c == '+' || c == '*' || c == '/';
 }
 
-int main(int argc, char** argv){
-    while(running){
+void skip_line(char c){
+    while(c != '\n'){
+            c = getchar();
+        }
+    line++;
+}
+
+int main(void){
+    int c = 0, num = 0;
+
+    while(running){ //currentlky never stopping
+       start:
+       c = getchar();
+       if(!isdigit_etc(c)){ //not allowed character
+        printf("Line %d: error at %c\n", line, c);
+        skip_line(c);
+        goto start;
+       }else{ //otherwise is allowed
+         if(full()){ //check if stack full
+            printf("Line %d: error at %c\n", line, c);
+            skip_line(c);
+            goto start;
+         }
+         
+         while(isdigit(c)){ //is a digit
+            num = num * 10 + (c - '0');
+            c = getchar();
+         }
+         
+         if(!push(num)){ // pushing to stack and error if no success
+            printf("Line %d: error at %d\n", line, num);
+            skip_line(c);
+            goto start;
+         }
         
+       }    
+
+       
+
     }
     return 0;
 }
